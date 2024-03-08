92cc1b6d7932f3d92b95f8aaa03c56125d4163b2
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-03-08 10:42:52 +0545

    Added styling to navbar elements for layout, positioning, and icon sizes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -16,8 +16,10 @@ const Navbar = () => {
         </div>
 
       <div className="nav-middle flex-div">
+        <div className="search-box flex-div">
         <input type="text" placeholder="Search"/>
         <img src={search_icon} alt="" />
+        </div>
       </div>
 
       <div className="nav-right flex-div">
