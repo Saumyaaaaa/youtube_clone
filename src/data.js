99797162b04194cae4167305c475ed8015b8499b
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-03-12 10:38:11 +0545

    Fixed moment import and added time ago functionality to video publication date

--- src/data.js
+++ src/data.js
@@ -1,2 +1,15 @@
 
 export const API_KEY='AIzaSyD9cYAJ4oUSAihf_qsNgDn9VOqZmH69duI'
+
+ export const value_converter=(value)=>{
+    if(value>=1000000)
+    {
+        return Math.floor(value/1000000)+"M"
+    }
+    else if(value>=1000){
+        return Math.floor(value/1000)+"k"
+    }
+    else{
+        return value
+    }
+}
