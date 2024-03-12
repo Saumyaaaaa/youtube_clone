e8753d85743867b4ad14bdae6768c00bd1705a73
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-03-12 11:03:13 +0545

    Implemented autoplay for the iframe and removed the title attribute from the embed code in the PlayVideo component.

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -7,10 +7,12 @@ import share from '../../assets/share.png'
 import save from '../../assets/save.png'
 import jack from '../../assets/jack.png'
 import user_profile from '../../assets/user_profile.jpg'
-const PlayVideo = () => {
+const PlayVideo = ({videoId}) => {
   return (
     <div className='play-video'>
-      <video src={video1} controls autoplay muted></video>
+      {/* <video src={video1} controls autoplay muted></video> */}
+      <iframe  src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} 
+      frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       <h3>Best YouTube Channel To Learn Web Development</h3>
       <div className="play-video-info">
         <p>1525 Views &bull; 2 days ago</p>
--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './Video.css'
 import PlayVideo from '../../Components/PlayVideo/PlayVideo'
 import Recommmended from '../../Components/Recommended/Recommmended'
+import { useParams } from 'react-router-dom';
 
 const Video = () => {
+  const{videoId,categoryId}=useParams()
   return (
     <div className='play-container'>
-      <PlayVideo/>
+      <PlayVideo videoId={videoId}/>
       <Recommmended/>
       
     </div>
