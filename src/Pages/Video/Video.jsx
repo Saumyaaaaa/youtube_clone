09b0183d13d823277c8b243848d915f320a3a11f
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-03-12 07:49:20 +0545

    Created Recommended component with thumbnails and video information

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Video.css'
 import PlayVideo from '../../Components/PlayVideo/PlayVideo'
+import Recommmended from '../../Components/Recommended/Recommmended'
 
 const Video = () => {
   return (
     <div className='play-container'>
       <PlayVideo/>
+      <Recommmended/>
       
     </div>
   )
