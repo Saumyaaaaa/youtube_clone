121c2bc28a722467f133fd01e12425bdf02a2437
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-03-13 07:28:08 +0545

    feat: Dynamically fetch YouTube video data and comments
    - Refactored PlayVideo component to dynamically fetch video data and comments from the YouTube Data API instead of hardcoding HTML elements.
    - Implemented useEffect hooks to fetch video data on component mount and to fetch additional data (channel info and comments) when video data is available.
    - Added conditional rendering to display video title, view count, publication date, likes, dislikes, channel info, video description, and comments dynamically.
    - Included error handling to prevent API calls when necessary data is not available.

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './PlayVideo.css'
 import video1 from '../../assets/video.mp4'
 import like from '../../assets/like.png'
@@ -7,23 +7,52 @@ import share from '../../assets/share.png'
 import save from '../../assets/save.png'
 import jack from '../../assets/jack.png'
 import user_profile from '../../assets/user_profile.jpg'
+import { API_KEY, value_converter } from '../../data'
+import moment from 'moment/moment'
 const PlayVideo = ({videoId}) => {
+    const [apiData,setApiData]=useState(null)
+    const[channelData,setChannelData]=useState(null)
+    const[commentData,setCommentData]=useState([])
+
+
+    const fetchVideoData=async()=>{
+        //Fetching Videos Data
+        const videoDetails_url=` https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY} `
+        await fetch(videoDetails_url).then(res=>res.json()).then(data=> setApiData(data.items[0]))
+    }
+    const fetchOtherData=async()=>{
+        if (!apiData) return
+        //Fetching  Channel Data
+        const channelData_url=`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY} `
+        await fetch(channelData_url).then(res=>res.json()).then(data=>setChannelData(data.items[0]))
+
+        //Fetching Comment Data
+        const comment_url=` https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=50&videoId=${videoId}&key=${API_KEY} `
+        await fetch(comment_url).then(res=>res.json()).then(data=>setCommentData(data.items))
+        
+    }
+    useEffect(()=>{
+        fetchVideoData()
+    },[])
+    useEffect(()=>{
+        fetchOtherData()
+    },[apiData])
   return (
     <div className='play-video'>
       {/* <video src={video1} controls autoplay muted></video> */}
       <iframe  src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} 
       frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      <h3>Best YouTube Channel To Learn Web Development</h3>
+      <h3>{apiData?apiData.snippet.title:"Title Here"}</h3>
       <div className="play-video-info">
-        <p>1525 Views &bull; 2 days ago</p>
+        <p>{apiData?value_converter(apiData.statistics.viewCount):"16k"} Views &bull; {apiData?moment(apiData.snippet.publishedAt).fromNow():""}</p>
         <div>
             <span>
                 <img src={like} alt="" />
-                125
+                {apiData?value_converter(apiData.statistics.likeCount):155}
             </span>
             <span>
                 <img src={dislike} alt="" />
-                2
+                
             </span>
             <span>
                 <img src={share} alt="" />
@@ -37,53 +66,34 @@ const PlayVideo = ({videoId}) => {
       </div>
       <hr />
       <div className="publisher">
-        <img src={jack} alt="" />
+        <img src={channelData?channelData.snippet.thumbnails.default.url:""} alt="" />
         <div>
-            <p>GreatStack</p>
-            <span>10M Subscribers</span>
+            <p>{apiData?apiData.snippet.channelTitle:""}</p>
+            <span>{channelData?value_converter(channelData.statistics.subscriberCount):"1M"}Subscribers</span>
         </div>
         <button>Subscribe</button>
       </div>
       <div className="vid-description">
-        <p>Channel that makes learning Easy</p>
-        <p>Subscribe GreatStack to watch More Tutorials on web development</p>
+        <p>{apiData?apiData.snippet.description.slice(0,250):"Description Here"}</p>
         <hr />
-        <h4>130 Comments</h4>
-        <div className="comment">
-            <img src={user_profile} alt="" />
-            <div>
-                <h3>Jack Nicholsan <span>1 day ago</span></h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium id quod adipisci ratione rem soluta dolores laudantium nostrum tempore dignissimos necessitatibus quaerat veritatis qui vitae, facilis nihil eos perspiciatis. Tenetur?</p>
-                <div className="comment-action">
-                    <img src={like} alt="" />
-                    <span>244</span>
-                    <img src={dislike} alt="" />
+        <h4>{apiData?value_converter(apiData.statistics.commentCount):102} Comments</h4>
+        {commentData.map((item,index)=>{
+            return (
+                <div key={index}className="comment">
+                <img src={item.snippet.topLevelComment.snippet.authorProfileImageUrl} alt="" />
+                <div>
+                    <h3>{item.snippet.topLevelComment.snippet.authorDisplayName} <span>{moment(item.snippet.publishedAt).fromNow()}</span></h3>
+                    <p>{item.snippet.topLevelComment.snippet.textDisplay}</p>
+                    <div className="comment-action">
+                        <img src={like} alt="" />
+                        <span>{value_converter(item.snippet.topLevelComment.snippet.likeCount)}</span>
+                        <img src={dislike} alt="" />
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className="comment">
-            <img src={user_profile} alt="" />
-            <div>
-                <h3>Jack Nicholsan <span>1 day ago</span></h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium id quod adipisci ratione rem soluta dolores laudantium nostrum tempore dignissimos necessitatibus quaerat veritatis qui vitae, facilis nihil eos perspiciatis. Tenetur?</p>
-                <div className="comment-action">
-                    <img src={like} alt="" />
-                    <span>244</span>
-                    <img src={dislike} alt="" />
-                </div>
-            </div>
-        </div>  <div className="comment">
-            <img src={user_profile} alt="" />
-            <div>
-                <h3>Jack Nicholsan <span>1 day ago</span></h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium id quod adipisci ratione rem soluta dolores laudantium nostrum tempore dignissimos necessitatibus quaerat veritatis qui vitae, facilis nihil eos perspiciatis. Tenetur?</p>
-                <div className="comment-action">
-                    <img src={like} alt="" />
-                    <span>244</span>
-                    <img src={dislike} alt="" />
-                </div>
-            </div>
-        </div>
+            )
+        })}
+       
       </div>
     </div>
   )
