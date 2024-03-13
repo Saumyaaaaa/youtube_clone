382f414add6f26b1e7f6c611037a74041888cfc6
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-03-13 08:34:04 +0545

    Resolved the issue

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -5,6 +5,8 @@ import like from '../../assets/like.png'
 import dislike from '../../assets/dislike.png'
 import share from '../../assets/share.png'
 import save from '../../assets/save.png'
+import jack from '../../assets/jack.png'
+import user_profile from '../../assets/user_profile.jpg'
 import { API_KEY, value_converter } from '../../data'
 import moment from 'moment/moment'
 import { useParams } from 'react-router-dom';
