bcf08664460f7fdb60e2d3e3c627c76035d0232e
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-03-13 08:12:58 +0545

    feat: Fetch and display recommended videos based on category
    - Integrated YouTube Data API to fetch recommended videos for a specified category.
    - Implemented React Router's Link component to create clickable links for each video, directing users to their individual pages.
    - Added logic to dynamically render video thumbnails, titles, channel names, and view counts.
    - Utilized `value_converter` function from data.js to format view counts for better readability.

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -9,7 +9,7 @@ const Video = () => {
   return (
     <div className='play-container'>
       <PlayVideo videoId={videoId}/>
-      <Recommmended/>
+      <Recommmended categoryId={categoryId}/>
       
     </div>
   )
